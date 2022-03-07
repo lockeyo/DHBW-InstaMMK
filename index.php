d8a2e9876b9dacde37d28e367f133fc2b873a185
Marcel Engelmann <lockeyo@users.noreply.github.com>
2022-03-07 15:21:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
-<?php
-    $name = "Welt";
-    $name = $_GET['name'];
-    echo "Hallo " . $name;
-?>
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DHBW - InstaMMK</title>
+  </head>
+  <body>
+        <nav>
+            <p>
+                <?php
+                    $name = $_GET['name'];
+                    echo "Halo " . $name;
+                ?>
+            </p>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Profil</a>
+                </li>
+            </ul>
+        </nav>
+
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/01/DHBW_MA_Logo.jpg" alt="Test" width="200px" />
+
+        <table>
+            <tr>
+                <td>Name</td>
+                <td>Alter</td>
+                <td>Note</td>
+            </tr>
+            <tr>
+                <td>Marcel</td>
+                <td>21</td>
+                <td>1,4</td>
+            </tr>
+        </table>
+
+  </body>
+</html>
