8497fe928cc9851b7716023ef0016803f4cc0323
Marcel Engelmann <lockeyo@users.noreply.github.com>
2022-03-07 17:34:29 +0100

    Update

--- index.php
+++ index.php
@@ -1,74 +1,36 @@
-
-
-    <?php
-        $servername = "localhost";
-        $username = "mmk21";
-        $password = "bjLHrcXGi74qL5nb73Th";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = "SELECT * FROM users";
-        $result = $conn->query($sql);
-    ?>
-    
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DHBW - InstaMMK</title>
-  </head>
-  <body>
+    <head>
+        <meta charset="utf-8">
+        <title>InstaMMk</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body>
 
+    <!-- Header -->
+    <header>
         <nav>
-            <p>
-                <?php
-                    $name = $_GET['name'];
-                    if($name == "Marcel"){
-                        echo $name . "'s Website";
-                    }
-                    else{
-                        echo "Hallo Welt";
-                    }
-                ?>
-            </p>
             <ul>
                 <li>
-                    <a href="index.php">Home</a>
+                    <a href="index.php">Timeline</a>
                 </li>
-                <li>
+                <li>   
                     <a href="profile.php">Profil</a>
                 </li>
             </ul>
         </nav>
 
-        <p>
-            <?php 
-                    echo $result[0]["userName"];
-            ?>
-        </p>
+    </header>    
 
+    <!-- Main -->
+    <main>
 
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/01/DHBW_MA_Logo.jpg" alt="Test" width="200px" />
+    </main>
 
-        <table>
-            <tr>
-                <td>Name</td>
-                <td>Alter</td>
-                <td>Note</td>
-            </tr>
-            <tr>
-                <td>Marcel</td>
-                <td>21</td>
-                <td>1,4</td>
-            </tr>
-        </table>
+    <!-- Footer -->
+    <footer>
 
-  </body>
+    </footer>
+
+    </body>
 </html>
